d4a308a217ce706a5cb7d391e43901c38d72018f
John Versus <johnvs.nagendra@gmail.com>
2023-03-21 10:27:11 +0530

    test fix: static metadata 🧪

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,29 +6,26 @@ const assetBasepath = process.env.NEXT_PUBLIC_BASEPATH + "/";
 const title = "Github Social Image Generator";
 const description =
   "Generate beautiful social media images for your Github repositories with the Github Social Image Generator.";
-const url = "https://tools.johnversus.dev/" + assetBasepath;
+const url = "https://tools.johnversus.dev/github-social-image-generator";
 const image =
-  "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/" +
-  assetBasepath;
+  "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/github-social-image-generator";
 // Metadata also to be updated in manifest and xml file
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Github Social Image Generator",
   description:
     "Generate beautiful social media images for your Github repositories with the Github Social Image Generator.",
   themeColor: "#ffffff",
-  manifest: assetBasepath + "favicon/site.webmanifest",
+  manifest: "github-social-image-generator/favicon/site.webmanifest",
   openGraph: {
     type: "website",
     siteName: "Github Social Image Generator",
-    url: "https://tools.johnversus.dev/" + assetBasepath,
+    url: "https://tools.johnversus.dev/github-social-image-generator",
     title: "Github Social Image Generator",
     description:
       "Generate beautiful social media images for your Github repositories with the Github Social Image Generator.",
     images: [
       {
-        url:
-          "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/" +
-          assetBasepath,
+        url: "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/github-social-image-generator",
         width: 1920,
         height: 960,
         alt: "Github Social Image Generator",
@@ -42,32 +39,30 @@ const metadata: Metadata = {
     description:
       "Generate beautiful social media images for your Github repositories with the Github Social Image Generator.",
     images: {
-      url:
-        "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/" +
-        assetBasepath,
+      url: "https://webapi.johnversus.dev/api/generateGithubSocial?repo_url=https://github.com/JohnVersus/github-social-image-generator",
       alt: "Github Social Image Generator",
     },
   },
   icons: {
     icon: [
       {
-        url: assetBasepath + "favicon/favicon-32x32.png",
+        url: "github-social-image-generator/favicon/favicon-32x32.png",
         type: "image/png",
         sizes: "32x32",
       },
       {
-        url: assetBasepath + "favicon/favicon-16x16.png",
+        url: "github-social-image-generator/favicon/favicon-16x16.png",
         type: "image/png",
         sizes: "16x16",
       },
     ],
 
-    shortcut: [assetBasepath + "/favicon/favicon.ico"],
+    shortcut: ["github-social-image-generator//favicon/favicon.ico"],
 
     apple: [
-      { url: assetBasepath + "favicon/apple-touch-icon.png" },
+      { url: "github-social-image-generator/favicon/apple-touch-icon.png" },
       {
-        url: assetBasepath + "favicon/apple-touch-icon.png",
+        url: "github-social-image-generator/favicon/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
@@ -75,9 +70,9 @@ const metadata: Metadata = {
   },
 };
 
-export async function generateMetadata(): Promise<Metadata> {
-  return metadata;
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   return metadata;
+// }
 
 export default function RootLayout({
   children,
@@ -112,37 +107,37 @@ export default function RootLayout({
         {/* <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={assetBasepath + "favicon/apple-touch-icon.png"}
+          href={"github-social-image-generator/favicon/apple-touch-icon.png"}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={assetBasepath + "favicon/favicon-32x32.png"}
+          href={"github-social-image-generator/favicon/favicon-32x32.png"}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={assetBasepath + "favicon/favicon-16x16.png"}
+          href={"github-social-image-generator/favicon/favicon-16x16.png"}
         />
         <link
           rel="manifest"
-          href={assetBasepath + "favicon/site.webmanifest"}
+          href={"github-social-image-generator/favicon/site.webmanifest"}
         />
         <link
           rel="mask-icon"
-          href={assetBasepath + "favicon/safari-pinned-tab.svg"}
+          href={"github-social-image-generator/favicon/safari-pinned-tab.svg"}
           color="#5bbad5"
         />
         <link
           rel="shortcut icon"
-          href={assetBasepath + "favicon/favicon.ico"}
+          href={"github-social-image-generator/favicon/favicon.ico"}
         />
         <meta name="msapplication-TileColor" content="#00916e" />
         <meta
           name="msapplication-config"
-          content={assetBasepath + "favicon/browserconfig.xml"}
+          content={"github-social-image-generator/favicon/browserconfig.xml"}
         />
         <meta name="theme-color" content="#1c1c1c"></meta> */}
       </head>
